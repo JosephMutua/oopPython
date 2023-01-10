596128e35a07cea318ca2464b6d017dbcfabb615
Joseph Mutua <josephmutua1738@gmail.com>
2023-01-10 09:46:54 +0300

    Class inheritance

--- employee.py
+++ employee.py
@@ -15,20 +15,71 @@ class Employee:
     def fullname (self):
         return '{} {}'.format(self.first, self.last)
     
-    def sal_raise (self):
+    def apply_raise (self):
         self.pay = (self.pay*self.raise_amt)
 
     @classmethod
     def set_emp_raise(cls,amount):
         cls.raise_amt = amount
 
+    @staticmethod
+    def is_workday (day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        else:
+            return True
+    
+
+#Create a subclass that inheretes attributes from the employee class
+class Developer (Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_language):
+        super().__init__(first, last, pay)
+        self.prog_language = prog_language
+
+# Creating a manager subclass that inherites from the employee class
+
+class Manager (Employee):
+    def __init__(self, first, last, pay, employees =None):
+        super().__init__(first, last, pay)
+
+        if employees == None:
+            self.employees = []
+
+        else:
+            self.employees = employees
+
+    def add_employee (self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+
+    def remove_employee (self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+
+    def print_employees(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+        
 
 
-
-emp = Employee('joseph','mutua',120000)
+emp = Developer('joseph','mutua',120000, 'Python')
+emp_1 = Developer('joe','Jane',120000, 'Jave')
 emp.set_emp_raise(1.05)
+print(emp.pay)
+emp.apply_raise()
+print(emp.pay)
 
-print (emp.raise_amt)
+mrg1 = Manager('Joh', 'Joe', 1220000, [emp])
+
+print(mrg1.email)
+
+mrg1.print_employees()
+mrg1.add_employee(emp_1)
+
+mrg1.print_employees()
 
 
 
